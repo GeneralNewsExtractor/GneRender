ffd6a97345d57ddeff70a5c0f8cf1dfa2e4a3c52
itskingname <>
2021-10-04 12:28:28 +0800

    add page visiability info

--- render.js
+++ render.js
@@ -30,6 +30,35 @@ function insert_visiability_info() {
             }
         }
     }
+
+    function sizes() {
+        let contentWidth = [...document.body.children].reduce( 
+          (a, el) => Math.max(a, el.getBoundingClientRect().right), 0) 
+          - document.body.getBoundingClientRect().x;
+      
+        return {
+          windowWidth:  document.documentElement.clientWidth,
+          windowHeight: document.documentElement.clientHeight,
+          pageWidth:    Math.min(document.body.scrollWidth, contentWidth),
+          pageHeight:   document.body.scrollHeight,
+          screenWidth:  window.screen.width,
+          screenHeight: window.screen.height,
+          pageX:        document.body.getBoundingClientRect().x,
+          pageY:        document.body.getBoundingClientRect().y,
+          screenX:     -window.screenX,
+          screenY:     -window.screenY - (window.outerHeight-window.innerHeight),
+        }
+    }
+
+    function insert_page_info() {
+        page_info = sizes()
+        node = document.createElement('meta')
+        node.setAttribute('name', 'page_visiability_info')
+        node.setAttribute('page_info', JSON.stringify(page_info))
+        document.getElementsByTagName('head')[0].appendChild(node)
+    }
+    
+    insert_page_info()
     body = get_body()
     iter_node(body)
 }
